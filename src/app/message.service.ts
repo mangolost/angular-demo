445584147d119634748c160cd86442b86b554699
mangolost <lichen0_0@163.com>
2019-01-27 12:10:22 +0800

    first commit

---
+++ src/app/message.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class MessageService {
+  messages: string[] = [];
+
+  constructor() { 
+
+  }
+
+  /**
+   * 
+   * @param message 
+   */
+  add(message: string) {
+    this.messages.push(message);
+  }
+ 
+  /**
+   * 
+   */
+  clear() {
+    this.messages = [];
+  }
+}
